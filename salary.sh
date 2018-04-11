af903900cee33bdc419850a53c775f843bc5110c
tittokvarghese <vinayanav@mca.ajce.in>
2018-04-11 10:16:07 +0530

    sucessfull

---
+++ salary.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Enter basic salary" 
+read bs
+
+
+hra=`expr $bs \* 10 / 100`
+echo "HRA=" $hra 
+da=`expr $bs \* 50 / 100`
+ta=250
+echo "DA=" $da
+total=$(( $da + $hra + $ta ))
+gross=$(( $bs + $total ))
+echo "Gross Salary = " $gross
